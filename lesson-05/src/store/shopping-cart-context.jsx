4188db83a5b312d5f697de50c00dc095964cc043
borscs <andras.borscs@opticruiter.com>
2025-11-28 15:09:41 +0100

    LESSON-04

---
+++ lesson-05/src/store/shopping-cart-context.jsx
@@ -0,0 +1,93 @@
+import {createContext, useReducer} from "react";
+import {DUMMY_PRODUCTS} from "../dummy-products.js";
+
+export const CartContext = createContext({
+	items: [],
+	addItemToCart: () => {
+	},
+	updateItemQuantity: () => {
+	},
+});
+
+function shoppingCartReducer(state, action) {
+	
+	if (action.type === 'ADD_ITEM') {
+		const updatedItems = [...state.items];
+		
+		const existingCartItemIndex = updatedItems.findIndex(
+			(cartItem) => cartItem.id === action.id
+		);
+		const existingCartItem = updatedItems[existingCartItemIndex];
+		
+		if (existingCartItem) {
+			const updatedItem = {
+				...existingCartItem,
+				quantity: existingCartItem.quantity + 1,
+			};
+			updatedItems[existingCartItemIndex] = updatedItem;
+		} else {
+			const product = DUMMY_PRODUCTS.find((product) => product.id === action.id);
+			updatedItems.push({
+				id: action.id,
+				name: product.title,
+				price: product.price,
+				quantity: 1,
+			});
+		}
+		
+		return {
+			items: updatedItems,
+		};
+	}
+	if(action.type === 'UPDATE_ITEM') {
+		const updatedItems = [...state.items];
+		const updatedItemIndex = updatedItems.findIndex(
+			(item) => item.id === action.productId
+		);
+		
+		const updatedItem = {
+			...updatedItems[updatedItemIndex],
+		};
+		
+		updatedItem.quantity += action.amount;
+		
+		if (updatedItem.quantity <= 0) {
+			updatedItems.splice(updatedItemIndex, 1);
+		} else {
+			updatedItems[updatedItemIndex] = updatedItem;
+		}
+		
+		return {
+			items: updatedItems,
+		};
+	}
+	
+	return state;
+}
+
+
+export default function CartContextProvider({children}) {
+	
+	const [shoppingCartState, shoppingDispatch] = useReducer(
+		shoppingCartReducer,
+		{
+			items: [],
+		}
+	)
+	
+	function handleAddItemToCart(id) {
+		shoppingDispatch({type: 'ADD_ITEM', id: id})
+	}
+	
+	function handleUpdateCartItemQuantity(productId, amount) {
+		shoppingDispatch({type: 'UPDATE_ITEM', productId: productId, amount: amount})
+	}
+	
+	const ctxValue = {
+		items: shoppingCartState.items,
+		addItemToCart: handleAddItemToCart,
+		updateItemQuantity: handleUpdateCartItemQuantity,
+	}
+	
+	return <CartContext.Provider value={ctxValue}>{children}</CartContext.Provider>;
+}
