98cb5c64a82653f5f6824e463138463d4f2d5f0b
borscs <andras.borscs@opticruiter.com>
2026-01-03 10:51:40 +0100

    lesson-08 hiba

---
+++ lesson-08/src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./componennts/Header.jsx";
+import Quiz from "./componennts/Quiz.jsx";
+
+function App() {
+	
+	return (
+		<>
+			<Header/>
+			<main>
+				<Quiz/>
+			</main>
+		</>
+	)
+}
+
+export default App;
