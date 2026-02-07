8ca2e2eef9a82c528d5e4c9cc77bfc6e98b0787f
borscs <andras.borscs@opticruiter.com>
2026-02-07 11:21:29 +0100

    lesson-12

---
+++ lesson-12/src/App.jsx
@@ -0,0 +1,17 @@
+import Header from './components/Header.jsx';
+import Login from './components/Login.jsx';
+import Signup from "./components/SingUp.jsx";
+import SateLogin from "./components/SateLogin.jsx";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <main>
+        <SateLogin />
+      </main>
+    </>
+  );
+}
+
+export default App;
